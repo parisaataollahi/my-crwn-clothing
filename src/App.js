378f66d082866844fae46059fa3a73b8b135b246
unknown <parisa1996.ataollahi@gmail.com>
2021-04-11 10:01:53 -0700

    add navigation-withRoterHOC

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import {Switch , Route} from 'react-router-dom';
+
+
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+
 
 function App() {
   return (
-    <div className="App">
-     <HomePage></HomePage>
+    <div>
+     <Switch>
+       <Route exact path='/' component={HomePage}/>
+       <Route  path='/shop/hats' component={HatsPage}/>
+     </Switch>
     </div>
   );
 }
