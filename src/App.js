ed63654a5ab13225a7d54b2a545a13f4f2031c28
unknown <parisa1996.ataollahi@gmail.com>
2021-04-19 00:38:38 -0700

    add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import {Switch , Route} from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import shopPage from './pages/shop/shop.component';
-import {Switch , Route} from 'react-router-dom';
+import Header from './components/header/header.component';
+
 
 
 
@@ -11,6 +14,7 @@ import {Switch , Route} from 'react-router-dom';
 function App() {
   return (
     <div>
+      <Header />
      <Switch>
        <Route exact path='/' component={HomePage}/>
        <Route  path='/shop' component={shopPage}/>
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './header.component.style.scss';
+
+//import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () =>(
+    <div className='header'>
+    <Link className='logo-container' to="/">
+    <div className='logo'>home</div>
+    </Link>
+    <div className='options'>
+     <Link className='option' to='/shop'>
+       SHOP
+     </Link>
+     <Link className='option' to='/shop'>
+       CONTACT
+     </Link>
+
+    </div>
+    </div>
+)
+export default Header;
